a05289ec818f494bda2e7d80f596087a063554f0
jannahmift <167733623+jannahmift@users.noreply.github.com>
2024-05-14 16:28:17 +0700

    panemu

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1488.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-b55ff326' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-8bb4b826' );
+});
+ });
